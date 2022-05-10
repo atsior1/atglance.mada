8f7fcbc7f482c927be09e3a79ef374c60cf1d527
andry.anthony <astro@node.com>
2022-05-10 14:24:01 +0200

    Add template for the voyage details

--- front-atglancemada/src/app/app.module.ts
+++ front-atglancemada/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MyMenuComponent } from './my-menu/my-menu.component';
+import { DestinationDescComponent } from './destination-desc/destination-desc.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyMenuComponent,
+    DestinationDescComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ front-atglancemada/src/app/destination-desc/destination-desc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-destination-desc',
+  templateUrl: './destination-desc.component.html',
+  styleUrls: ['./destination-desc.component.css']
+})
+export class DestinationDescComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
